e0f0872c10ab49671c7d68cda9968a0cfc1e79b8
jdejong <jdejong@sparknettech.com>
2010-11-26 16:24:10 -0500

    Updated files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saasy::Application.config.secret_token = '86f115db8e12d2c8967e9d09b602bd702512ad380ea8a95aee37c093a4d6bf00d0dccc9dc0cfac4b3edf846181f18f1faca72247e66fed3ba15b66d2c6871b58'
